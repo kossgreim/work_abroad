7e739a871c23375ef42a1025da04de00b5d3eb43
swtnext <swtnext@gmail.com>
2016-06-09 23:30:44 +0200

    revise assets

--- Gemfile
+++ Gemfile
@@ -36,6 +36,7 @@ gem 'devise'
 gem 'slim-rails'
 gem 'figaro'
 gem 'pundit'
+gem "font-awesome-rails"
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  root to: 'frontend#index'
-
   devise_for :users, controllers: {sessions: "users/sessions"}
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  authenticated :user do
+    root 'dashboard#index', as: :authenticated_root
+  end
+
+  root to: 'frontend#index'
 end
