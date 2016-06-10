b771aadbe820b3131f29a6c169a955fe5966476f
swtnext <swtnext@gmail.com>
2016-06-10 12:50:46 +0200

    set up rspec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe User, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#full_name' do
+    let(:user) { build(:user) }
+    it 'joins up first and last name' do
+      expect(user.full_name).to eq("#{user.first_name} #{user.last_name}")
+    end
+  end
 end
